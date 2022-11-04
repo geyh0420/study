706c3243850ce0084233fbcb8a4dda285132e23a
zer0921 <18768114067@126.com>
2022-11-04 19:23:35 +0800

    init commit

---
+++ spider.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+TMP_FILE=tmp.txt
+
+function download_file() {
+  local baseurl=$1
+  local url=$2
+  local name=$3
+  curl -s "${baseurl}${url}" > "$name"
+}
+
+function download_dir() {
+  local baseurl=$1
+  local url=$2
+  local name=$3
+
+  local baseurl_="${baseurl}${url}"
+  local url_=
+  local name_=
+
+  curl -s "$baseurl_" > index.html
+  cat index.html | grep href | grep -v '\.\.' | sed -e 's;<a href=";;' -e 's;</a>.*;;' -e 's;">;\n;' > $TMP_FILE
+  while read -r url_
+  do
+    read -r name_
+    if [[ "$url_" =~ .*\/$ ]]
+    then
+      mkdir "$name_" && cd "$name_"
+      download_dir "$baseurl_" "$url_" "$name_"
+      cd -
+    else
+      download_file "$baseurl_" "$url_" "$name_"
+    fi
+  done < $TMP_FILE
+}
+
+
+baseurl=http://lingxi.live/
+url=123
+name=123
+mkdir "$name" && cd "$name"
+download_dir "$baseurl" "$url" "$name"
